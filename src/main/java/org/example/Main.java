da82a2373d861ebc5daf20280485efa9586d3ec1
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-20 00:04:12 +0600

    feature: first statement and its execution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,22 @@ package org.example;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
         Class<Driver> driverClass = Driver.class;
-        Connection connection = ConnectionManager.open();
+        String str = "CREATE TABLE IF NOT EXISTS cars(" +
+                "id INT PRIMARY KEY," +
+                "name VARCHAR," +
+                "speed INT)";
+
+        String str2 = "DROP TABLE IF EXISTS cars";
         try {
-            System.out.println(connection.getTransactionIsolation());
+            Connection connection = ConnectionManager.open();
+            Statement statement = connection.createStatement();
+            boolean temp = statement.execute(str2);
+            System.out.println(temp);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
